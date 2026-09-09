eca832286627e2d61e284b5a978dc355a4aab1b8
Frozen <Frozen@user.noreply.gitee.com>
2026-09-09 15:41:30 +0800

    fix: use supported scheduled task logon type

--- scripts/install-reachy-video-relay-task.ps1
+++ scripts/install-reachy-video-relay-task.ps1
@@ -19,7 +19,7 @@ $settings = New-ScheduledTaskSettingsSet `
     -RestartCount 999 `
     -RestartInterval (New-TimeSpan -Minutes 1) `
     -ExecutionTimeLimit ([TimeSpan]::Zero)
-$principal = New-ScheduledTaskPrincipal -UserId $principalUser -LogonType InteractiveToken -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserId $principalUser -LogonType Interactive -RunLevel Highest
 
 Register-ScheduledTask `
     -TaskName $taskName `
